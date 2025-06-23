cd4848f1ce9eb16e8bc30682cdf1db7792a7c014
VPBerestenko <valdislaf@gmail.com>
2025-06-23 15:32:05 +0300

    commit

--- build_light_log.bat
+++ build_light_log.bat
@@ -1,6 +1,6 @@
 del *.exe
 gfortran -c win_types.f90 string_utils.f90 standard.f90 win_api.f90 gui_helpers.f90
-gfortran -o win.exe win.f90 win_types.o string_utils.o standard.o win_api.o gui_helpers.o -mwindows -luser32 -lgdi32
+gfortran -o win.exe win.f90 win_types.o string_utils.o standard.o win_api.o gui_helpers.o -luser32 -lgdi32
 del *.o
 del *.mod
 win
