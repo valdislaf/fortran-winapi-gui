d2d6b455676650b512a0124ab39754a6719831d8
VPBerestenko <valdislaf@gmail.com>
2025-06-24 12:25:33 +0300

    commit

--- win.f90
+++ win.f90
@@ -12,8 +12,7 @@ program WinMain
   type(AppData), target :: appDataInst
   type(c_ptr) :: appDataPtr
   type(ptr) :: hButton
-  character(kind=char), allocatable, target :: windowTitleW(:), classNameW(:), panelClassW(:), buttonTextW(:), classButtonW(:)  
-  integer(i_ptr) :: id_temp 
+  character(kind=char), allocatable, target :: windowTitleW(:), classNameW(:), panelClassW(:), buttonTextW(:), classButtonW(:)    
   character(kind=char), allocatable, target :: iconPathW(:), cursorPathW(:)
   integer(int), parameter :: panelWidth = 800 / 10
   ! --- Строки ---
@@ -36,23 +35,25 @@ program WinMain
   ! --- Главное окно ---
   call create_main_window(hwnd, hInstance, appDataPtr, hBrush, wcx, regResult, &
                         classNameW, windowTitleW, iconPathW, cursorPathW)
-
   call ShowWindow(hwnd, SW_SHOW)
   call UpdateWindow(hwnd)
 
   ! --- Панель ---
   call create_panel_window(appDataInst%hPanel, hwnd, hInstance, hPanelBrush, wcxPanel, regResult, panelClassW, panelWidth, 600)
-  call SetWindowLongPtrW(hwnd, -21, transfer(c_loc(appDataInst), 0_i_ptr))
   call ShowWindow(appDataInst%hPanel, SW_SHOW)
   call UpdateWindow(appDataInst%hPanel)  
 
-  ! --- Кнопка ---
-  
-  id_temp = transfer(ID_BUTTON1, 0_i_ptr)
-  call create_button(hButton, appDataInst%hPanel, hInstance, buttonTextW, classButtonW, id_temp, regResult)
+  ! --- Кнопка ---   
+  call create_button(hButton, appDataInst%hPanel, hInstance, buttonTextW, classButtonW, transfer(ID_BUTTON1, 0_i_ptr), regResult)
   call ShowWindow(hButton, SW_SHOW)
-  call UpdateWindow(hButton)
-
+  call UpdateWindow(hButton)   
+  
+  ! Устанавливаем пользовательские данные (AppData) в окно.
+  ! Это необходимо делать *после* создания панели и всех её компонентов,
+  ! чтобы структура данных содержала актуальные указатели (например, на hPanel).
+  ! Используется в WndProc для доступа к этим данным через GetWindowLongPtrW.
+  call SetWindowLongPtrW(hwnd, -21, transfer(c_loc(appDataInst), 0_i_ptr))
+  
   ! --- Цикл сообщений ---
   do while (GetMessageW(c_loc(msg_inst), nullptr, 0, 0) > 0)
     call TranslateMessage(c_loc(msg_inst))
